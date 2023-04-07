4c31260c5cde94e3089efdf79544b06d4aabbf75
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2023-04-07 14:17:23 -0400

    Initial Commit

---
+++ KayPetDiary/KayPetDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KayPetDiaryApp.swift
+//  KayPetDiary
+//
+//  Created by Panchi on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct KayPetDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
