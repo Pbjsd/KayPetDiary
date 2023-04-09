171bcd9074a3dfd89495380bdca95ec4f5ee588f
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2023-04-08 20:53:56 -0400

    4/8: Fixed error of SF symbol

--- KayPetDiary/ContentView.swift
+++ KayPetDiary/ContentView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+  @State private var showingSheet = false
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  var body: some View {
+
+    VStack {
+      HStack {
+          Text("Welcome, Sandy!")
+
+        Button {
+          showingSheet.toggle()
+        } label: {
+          Image(systemName: "arrowshape.right")
+        }
+
+      }
+      .sheet(isPresented: $showingSheet) {
+        OnboardingScreen()
     }
+      Spacer()
+    }
+  }
+
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+    }
+  }
 }
---
+++ KayPetDiary/OnboardingScreen.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingScreen.swift
+//  KayPetDiary
+//
+//  Created by Panchi on 4/7/23.
+//
+
+import SwiftUI
+
+struct OnboardingScreen: View {
+  @Environment(\.dismiss) var dismiss
+  
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreen()
+    }
+}
